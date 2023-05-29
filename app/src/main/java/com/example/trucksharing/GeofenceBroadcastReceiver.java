4c28c50a9cbe6e403f6da6ec062447d4a70f5cb8
Navinraaj <117885210+Navinraaj@users.noreply.github.com>
2023-05-29 15:52:37 +1000

    Commit

---
+++ app/src/main/java/com/example/trucksharing/GeofenceBroadcastReceiver.java
@@ -0,0 +1,81 @@
+package com.example.trucksharing;
+
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Build;
+import android.util.Log;
+
+import androidx.core.app.NotificationCompat;
+import androidx.core.app.NotificationManagerCompat;
+
+import com.example.trucksharing.activity.MainActivity;
+import com.google.android.gms.location.Geofence;
+import com.google.android.gms.location.GeofencingEvent;
+
+public class GeofenceBroadcastReceiver extends BroadcastReceiver {
+
+    private static final String TAG = "GeofenceBroadcastRecv";
+    private static final String CHANNEL_ID = "Geofence Channel";
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        GeofencingEvent geofencingEvent = GeofencingEvent.fromIntent(intent);
+        if (geofencingEvent.hasError()) {
+            String errorMessage = Integer.toString(geofencingEvent.getErrorCode());
+            Log.e(TAG, errorMessage);
+            return;
+        }
+
+        int geofenceTransition = geofencingEvent.getGeofenceTransition();
+
+        if (geofenceTransition == Geofence.GEOFENCE_TRANSITION_ENTER) {
+            Log.d(TAG, "Geofence ENTER triggered");
+            sendNotification(context, "You are near Deakin Burwood Campus");
+        } else if (geofenceTransition == Geofence.GEOFENCE_TRANSITION_EXIT) {
+            Log.d(TAG, "Geofence EXIT triggered");
+            sendNotification(context, "You have left the vicinity of Deakin Burwood Campus");
+        } else {
+            Log.e(TAG, "Invalid Geofence Transition");
+        }
+    }
+
+    private void sendNotification(Context context, String message) {
+        createNotificationChannel(context);
+
+        Intent intent = new Intent(context, MainActivity.class);
+        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+
+        PendingIntent pendingIntent;
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            pendingIntent = PendingIntent.getActivity(context, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT | PendingIntent.FLAG_IMMUTABLE);
+        } else {
+            pendingIntent = PendingIntent.getActivity(context, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT);
+        }
+
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(context, CHANNEL_ID)
+                .setContentTitle("Geofence Alert")
+                .setContentText(message)
+                .setPriority(NotificationCompat.PRIORITY_HIGH)
+                .setContentIntent(pendingIntent)
+                .setAutoCancel(true);
+
+        NotificationManagerCompat notificationManager = NotificationManagerCompat.from(context);
+        notificationManager.notify(0, builder.build());
+    }
+
+    private void createNotificationChannel(Context context) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            CharSequence name = "Geofence Channel";
+            String description = "Channel for Geofence alerts";
+            int importance = NotificationManager.IMPORTANCE_HIGH;
+            NotificationChannel channel = new NotificationChannel(CHANNEL_ID, name, importance);
+            channel.setDescription(description);
+            NotificationManager notificationManager = context.getSystemService(NotificationManager.class);
+            notificationManager.createNotificationChannel(channel);
+        }
+    }
+}
---
+++ app/src/main/java/com/example/trucksharing/activity/MainActivity.java
@@ -0,0 +1,192 @@
+package com.example.trucksharing.activity;
+
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+
+import android.Manifest;
+import android.app.PendingIntent;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.example.trucksharing.R;
+import com.example.trucksharing.data.DatabaseHelper;
+import com.example.trucksharing.model.Truck;
+import com.example.trucksharing.util.Util;
+
+import java.util.List;
+
+
+import com.example.trucksharing.GeofenceBroadcastReceiver;
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.location.Geofence;
+import com.google.android.gms.location.GeofenceStatusCodes;
+import com.google.android.gms.location.GeofencingClient;
+import com.google.android.gms.location.GeofencingRequest;
+import com.google.android.gms.location.LocationServices;
+
+
+public class MainActivity extends AppCompatActivity {
+    DatabaseHelper database;
+
+    private GeofencingClient geofencingClient;
+    private PendingIntent geofencePendingIntent;
+    private static final int REQUEST_FINE_LOCATION = 1234;
+    EditText editTextUsername;
+    EditText editTextPassword;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        database = new DatabaseHelper(this);
+
+        List<Truck> trucks = database.fetchAllTrucks();
+        if (trucks.size() == 0) {
+            // Add Trucks to database
+            String[] imageNames = {"ford", "chevrolet", "hyundai", "nissan"};
+            String[] names = {"Ford", "Chevrolet", "Hyundai", "Nissan"};
+            String[] status = {
+                    "3.5L EcoBoost® High-Output V6",
+                    "6.5L EcoBoost® High-Performance",
+                    "4.5L EcoBoost® High-Output V8",
+                    "2.0L EcoBoost® High-Output V6"
+            };
+            for (int i = 0; i < imageNames.length; i++) {
+                Truck truck = new Truck(imageNames[i], names[i], status[i]);
+                database.insertTruck(truck);
+            }
+        }
+
+           ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 1234);
+
+        editTextUsername = findViewById(R.id.editTextUsername);
+        editTextPassword = findViewById(R.id.editTextSignupPassword);
+        geofencingClient = LocationServices.getGeofencingClient(this);
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[] {Manifest.permission.ACCESS_FINE_LOCATION}, REQUEST_FINE_LOCATION);
+        } else {
+            createGeofences();
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        // Check if the permission request was successful
+        if (requestCode == 1234) {
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                // The permission was granted, you can now proceed with geofencing operations
+                createGeofences();
+            } else {
+                // The permission was denied, you should handle this case
+                Toast.makeText(MainActivity.this, "Fine location permission required for geofencing", Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+
+
+
+    private void createGeofences() {
+        GeofencingRequest geofencingRequest = createGeofence();
+
+        try {
+            geofencingClient.addGeofences(geofencingRequest, getGeofencePendingIntent())
+                    .addOnSuccessListener(aVoid -> {
+                        Toast.makeText(MainActivity.this, "Geofence added", Toast.LENGTH_SHORT).show();
+                        Log.i(MainActivity.this.toString(), "Geofence added successfully");
+                    })
+                    .addOnFailureListener(e -> {
+                        String message = "Failed to add geofence";
+                        if (e instanceof ApiException) {
+                            ApiException apiException = (ApiException) e;
+                            switch (apiException.getStatusCode()) {
+                                case GeofenceStatusCodes.GEOFENCE_NOT_AVAILABLE:
+                                    message = "Geofence not available";
+                                    break;
+                                case GeofenceStatusCodes.GEOFENCE_TOO_MANY_GEOFENCES:
+                                    message = "Too many Geofences";
+                                    break;
+                                case GeofenceStatusCodes.GEOFENCE_TOO_MANY_PENDING_INTENTS:
+                                    message = "Too many pending intents";
+                                    break;
+                            }
+                        }
+                        Log.e(MainActivity.this.toString(), message, e);
+                        Toast.makeText(MainActivity.this, message, Toast.LENGTH_SHORT).show();
+                    });
+        } catch (SecurityException e) {
+            Log.e(MainActivity.this.toString(), "Fine location permission not granted", e);
+            Toast.makeText(MainActivity.this, "Fine location permission not granted", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+
+    private GeofencingRequest createGeofence() {
+        Geofence geofence = new Geofence.Builder()
+                .setRequestId("Deakin_Burwood_Campus")
+                .setCircularRegion(-37.841265, 145.113075, 1000)
+                .setExpirationDuration(Geofence.NEVER_EXPIRE)
+                .setTransitionTypes(Geofence.GEOFENCE_TRANSITION_ENTER | Geofence.GEOFENCE_TRANSITION_EXIT)
+                .build();
+
+        GeofencingRequest.Builder builder = new GeofencingRequest.Builder();
+        builder.setInitialTrigger(GeofencingRequest.INITIAL_TRIGGER_ENTER);
+        builder.addGeofence(geofence);
+        return builder.build();
+    }
+
+
+
+    private PendingIntent getGeofencePendingIntent() {
+        if (geofencePendingIntent != null) {
+            return geofencePendingIntent;
+        }
+        Intent intent = new Intent(this, GeofenceBroadcastReceiver.class);
+        geofencePendingIntent = PendingIntent.getBroadcast(this, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT);
+        return geofencePendingIntent;
+    }
+
+    public void loginClick(View view) {
+
+        String username = editTextUsername.getText().toString();
+        String password = editTextPassword.getText().toString();
+
+        if (TextUtils.isEmpty(username) || TextUtils.isEmpty(password)) {
+            AlertDialog alertDialog = new AlertDialog.Builder(MainActivity.this).create();
+            alertDialog.setTitle("");
+            alertDialog.setMessage("Please enter username and password!");
+            alertDialog.setButton(AlertDialog.BUTTON_NEUTRAL, "OK",
+                    (dialog, which) -> dialog.dismiss());
+            alertDialog.show();
+            return;
+        }
+
+        int userId = database.fetchUser(username, password);
+        if (userId >= 0) {
+            Intent homeIntent = new Intent(MainActivity.this, HomeActivity.class);
+            homeIntent.putExtra(Util.USER_ID, userId);
+            startActivity(homeIntent);
+        } else {
+            Log.d(MainActivity.this.toString(), "The user does not exist");
+            AlertDialog alertDialog = new AlertDialog.Builder(MainActivity.this).create();
+            alertDialog.setTitle("Oops!");
+            alertDialog.setMessage("The user does not exist");
+            alertDialog.setButton(AlertDialog.BUTTON_NEUTRAL, "OK",
+                    (dialog, which) -> dialog.dismiss());
+            alertDialog.show();
+        }
+    }
+
+    public void signupClick(View view) {
+        Intent intent = new Intent(MainActivity.this, SignupActivity.class);
+        startActivity(intent);
+    }
+}
