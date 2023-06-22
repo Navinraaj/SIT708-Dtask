601fa2682bbb2ec1d85bb01a926cabe67ae258ed
Navinraaj <117885210+Navinraaj@users.noreply.github.com>
2023-06-23 01:14:08 +1000

    Updated Comments

--- app/src/main/java/com/example/trucksharing/adapter/OrderRecyclerViewAdapter.java
+++ app/src/main/java/com/example/trucksharing/adapter/OrderRecyclerViewAdapter.java
@@ -19,10 +19,15 @@ import com.example.trucksharing.model.Order;
 
 import java.util.List;
 
+// This class acts as the bridge between the RecyclerView and the data source (the Order list in this case).
 public class OrderRecyclerViewAdapter extends RecyclerView.Adapter<OrderRecyclerViewAdapter.ViewHolder> {
-    private List<Order> orderList; // List of orders to be displayed
-    private Context context; // The activity or application context
+    // This list holds the data that will be displayed in the RecyclerView.
+    private List<Order> orderList;
+    // The activity or application context is needed to retrieve resources and to
+    // start new activities.
+    private Context context;
 
+    // Constructor receives the data source and context.
     public OrderRecyclerViewAdapter(List<Order> orderList, Context context) {
         this.orderList = orderList;
         this.context = context;
@@ -31,17 +36,19 @@ public class OrderRecyclerViewAdapter extends RecyclerView.Adapter<OrderRecycler
     @NonNull
     @Override
     public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        // Inflate the layout for each row of the RecyclerView
+        // Inflate the layout for each row of the RecyclerView from the XML layout file.
         View itemView = LayoutInflater.from(context).inflate(R.layout.order_row, parent, false);
+        // Return a new ViewHolder instance and pass the inflated layout to it.
         return new ViewHolder(itemView);
     }
 
     @Override
     public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
-        // Get the current order
+        // Get the current order that will be displayed in this row.
         Order order = orderList.get(position);
 
-        // Check if the order has an image name and update the ImageView accordingly
+        // Check if the order has an image name and update the ImageView accordingly.
+        // If not, set a placeholder image.
         if (order.getImageName().length() > 0) {
             int resourceId = context.getResources().getIdentifier(order.getImageName(), "drawable",
                     context.getPackageName());
@@ -52,29 +59,34 @@ public class OrderRecyclerViewAdapter extends RecyclerView.Adapter<OrderRecycler
             holder.imageView.setImageResource(resourceId);
         }
 
-        // Set the text fields
+        // Set the text fields with the data from the current order.
         holder.textViewName.setText(order.getReceiverName());
         holder.textViewPickupTime.setText(order.getPickupDate() + " " + order.getPickupTime());
 
-        // Set a click listener for the order row
+        // Set a click listener for the order row.
+        // When a row is clicked, the detailed view of the order will be shown in the
+        // OrdersActivity.
         holder.setItemClickListener(new ItemClickListener() {
             @Override
             public void onClick(View view, int position) {
                 if (context instanceof OrdersActivity) {
                     Log.d(this.toString(), "Postition = " + String.valueOf(position));
-                    ((OrdersActivity)context).showOrderDetails(orderList.get(position));
+                    ((OrdersActivity) context).showOrderDetails(orderList.get(position));
                 }
             }
         });
 
-//        holder.shareButton.setOnClickListener((View.OnClickListener) this);
+        // holder.shareButton.setOnClickListener((View.OnClickListener) this);
     }
 
     @Override
     public int getItemCount() {
+        // Return the total count of items in the list.
         return orderList.size();
     }
 
+    // This class holds the views that will be recycled and reused as the user
+    // scrolls.
     public class ViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
 
         ImageView imageView;
@@ -87,13 +99,16 @@ public class OrderRecyclerViewAdapter extends RecyclerView.Adapter<OrderRecycler
         public ViewHolder(@NonNull View itemView) {
             super(itemView);
 
+            // Get the views from the layout.
             imageView = itemView.findViewById(R.id.imageViewOrder);
             textViewName = itemView.findViewById(R.id.textViewReceiverName);
             textViewPickupTime = itemView.findViewById(R.id.textViewPickupTime);
-//            shareButton.setOnClickListener(this);
+            // shareButton.setOnClickListener(this);
+            // Set the click listener for the entire row.
             itemView.setOnClickListener(this);
         }
 
+        // This method is used to set a custom click listener.
         public void setItemClickListener(ItemClickListener itemClickListener) {
             this.itemClickListener = itemClickListener;
         }
@@ -101,12 +116,15 @@ public class OrderRecyclerViewAdapter extends RecyclerView.Adapter<OrderRecycler
         @Override
         public void onClick(View v) {
 
+            // Check which view was clicked.
             if (v == shareButton) {
+                // If it was the shareButton, start the share content functionality.
                 if (context instanceof OrdersActivity) {
-                    ((OrdersActivity)context).shareContent(orderList.get(getAdapterPosition()));
+                    ((OrdersActivity) context).shareContent(orderList.get(getAdapterPosition()));
                 }
             } else {
-                itemClickListener.onClick(v,getAdapterPosition());
+                // If it was the row, call the custom click listener that was set.
+                itemClickListener.onClick(v, getAdapterPosition());
             }
         }
     }
--- app/src/main/java/com/example/trucksharing/adapter/TruckRecyclerViewAdapter.java
+++ app/src/main/java/com/example/trucksharing/adapter/TruckRecyclerViewAdapter.java
@@ -17,12 +17,16 @@ import com.example.trucksharing.model.Truck;
 
 import java.util.List;
 
+// This class acts as the bridge between the RecyclerView and the data source (the Truck list in this case).
 public class TruckRecyclerViewAdapter extends RecyclerView.Adapter<TruckRecyclerViewAdapter.ViewHolder> {
 
+    // This list holds the data that will be displayed in the RecyclerView.
+    private List<Truck> truckList;
+    // The activity or application context is needed to retrieve resources and to
+    // start new activities.
+    private Context context;
 
-    private List<Truck> truckList; // List of trucks to be displayed
-    private Context context; // The activity or application context
-
+    // Constructor receives the data source and context.
     public TruckRecyclerViewAdapter(List<Truck> truckList, Context context) {
         this.truckList = truckList;
         this.context = context;
@@ -31,27 +35,34 @@ public class TruckRecyclerViewAdapter extends RecyclerView.Adapter<TruckRecycler
     @NonNull
     @Override
     public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        // Inflate the layout for each row of the RecyclerView from the XML layout file.
         View itemView = LayoutInflater.from(context).inflate(R.layout.truck_row, parent, false);
+        // Return a new ViewHolder instance and pass the inflated layout to it.
         return new ViewHolder(itemView);
     }
 
     @Override
     public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
-        // Get the current truck
+        // Get the current truck that will be displayed in this row.
         Truck truck = truckList.get(position);
+        // Get the resource id for the image from the truck object and set the image.
         int resourceId = context.getResources().getIdentifier(truck.getImageName(), "drawable",
                 context.getPackageName());
         holder.imageView.setImageResource(resourceId);
+        // Set the text fields with the data from the current truck.
         holder.textViewName.setText(truck.getName());
         holder.textViewStatus.setText(truck.getStatus());
     }
 
     @Override
     public int getItemCount() {
+        // Return the total count of items in the list.
         return truckList.size();
     }
 
-    public class ViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener  {
+    // This class holds the views that will be recycled and reused as the user
+    // scrolls.
+    public class ViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
 
         ImageView imageView;
         TextView textViewName;
@@ -61,18 +72,22 @@ public class TruckRecyclerViewAdapter extends RecyclerView.Adapter<TruckRecycler
         public ViewHolder(@NonNull View itemView) {
             super(itemView);
 
+            // Get the views from the layout.
             imageView = itemView.findViewById(R.id.imageViewTruck);
             textViewName = itemView.findViewById(R.id.textViewReceiverName);
             textViewStatus = itemView.findViewById(R.id.textViewPickupTime);
             shareButton = itemView.findViewById(R.id.truckRowSharingButton);
 
+            // Set the click listener for the share button.
             shareButton.setOnClickListener((View.OnClickListener) this);
         }
 
         @Override
         public void onClick(View view) {
+            // If the share button was clicked, start the share content functionality in the
+            // HomeActivity.
             if (context instanceof HomeActivity) {
-                ((HomeActivity)context).shareContent(truckList.get(getAdapterPosition()));
+                ((HomeActivity) context).shareContent(truckList.get(getAdapterPosition()));
             }
         }
     }
